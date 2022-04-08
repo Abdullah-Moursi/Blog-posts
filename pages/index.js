3ecdef3e53b440eb1bcf32ce42429436a2db30a4
Abdullah Moursi <moursiabdullah@gmail.com>
2022-04-09 00:02:10 +0200

    dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -35,3 +35,4 @@ export async function getStaticProps() {
     props: { posts },
   }
 }
+
--- services/index.js
+++ services/index.js
@@ -37,6 +37,40 @@ export const getPosts = async () => {
   return result.postsConnection.edges
 }
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        author {
+          bio
+          name
+          id
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        categories {
+          name
+          slug
+        }
+        content {
+          raw
+        }
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug })
+  return result.post
+}
+
 export const getRecentPosts = async () => {
   const query = gql`
   query GetPostDetails() {
@@ -58,7 +92,7 @@ export const getRecentPosts = async () => {
   return result.posts
 }
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
@@ -78,18 +112,18 @@ export const getSimilarPosts = async () => {
     }
   `
 
-  const result = await request(graphqlAPI, query)
+  const result = await request(graphqlAPI, query, {categories, slug})
   return result.posts
 }
 
 export const getCategories = async () => {
   const query = gql`
-  query GetCategories {
-    categories {
-      name
-      slug
+    query GetCategories {
+      categories {
+        name
+        slug
+      }
     }
-  }
   `
   const result = await request(graphqlAPI, query)
   return result.categories
