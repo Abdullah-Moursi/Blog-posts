57296c8a2c9b7418e4116f1195b465972fd35771
Abdullah Moursi <moursiabdullah@gmail.com>
2022-04-10 00:56:05 +0200

    aurhor

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <div>Author</div>
+    <div className="relative mt-20 mb-8 rounded-lg bg-black bg-opacity-20 p-12 text-center ">
+      <div className="absolute left-0 right-0 -top-14 ">
+        <Image
+          unoptimized
+          src={author.photo.url}
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="rounded-full align-middle"
+        />
+      </div>
+      <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+      <p className="text-lg text-white">{author.bio}</p>
+    </div>
   )
 }
 
